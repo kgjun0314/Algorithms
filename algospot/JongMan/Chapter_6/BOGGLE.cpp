a413c3ade1595099fc1d8ff4af3a90649a1eb9d0
kgjun0314 <unicorn9717@gmail.com>
2022-12-28 21:13:51 +0900

    Edit BOGGLE.cpp

--- algospot/JongMan/Chapter_6/BOGGLE.cpp
+++ algospot/JongMan/Chapter_6/BOGGLE.cpp
@@ -1,81 +1,104 @@
 #include <iostream>
+#include <vector>
 #include <string>
+#include <cstring>
+
 using namespace std;
 
-int dx[8] = {-1, -1, -1, 1, 1, 1, 0, 0};
-int dy[8] = {-1, 0, 1, -1, 0, 1, -1, 1};
-char Board[5][5];
-bool flag = false;
+int C, N;
+int board[5][5];
+int dp[5][5][10];
 
-bool InRange(int x, int y)
+int dx[8] = {-1, -1, -1, 0, 1, 1, 1, 0};
+int dy[8] = {1, 0, -1, -1, -1, 0, 1, 1};
+
+bool isRange(int x, int y) 
 {
-    if(x < 0 || y < 0 || x >= 5 || y >= 5)
-        return false;
-    return true;
+    return (0 <= x && x < 5) && (0 <= y && y < 5);
 }
 
-bool HasWord(int x, int y, const string& Word)
+int solve(const string & s, int x, int y, int index) 
 {
-    if(!InRange(x, y))
-        return false;
-    
-    if(Board[x][y] != Word[0])
-        return false;
-    
-    if(Word.size() == 1)
-        return true;
-    
-    for(int i = 0; i < 8; i++)
-    {
-        int Del_x = x + dx[i], Del_y = y + dy[i];
-        if(HasWord(Del_x, Del_y, Word.substr(1)))
-            return true;
+    if(index >= s.length()) 
+	{
+        return 1;
+    }
+
+    if(!isRange(x, y) || board[x][y] != s[index]) 
+	{
+        return 0;
+    }
+
+    if(dp[x][y][index] != -1) 
+	{
+        return dp[x][y][index];
+    }
+
+    dp[x][y][index] = 0;
+
+    for(int i = 0; i < 8; i++) 
+	{
+        int nx = x + dx[i];
+        int ny = y + dy[i];
+        int nextIndex = index + 1;
+
+        if(solve(s, nx, ny, nextIndex))
+		{
+            dp[x][y][index] = 1;
+            return dp[x][y][index];
+        }
+    }
+
+    return dp[x][y][index];
+}
+
+bool solution(const string & s) 
+{
+    for(int i = 0; i < 5; i++) 
+	{
+        for(int j = 0; j < 5; j++) 
+		{
+            if(solve(s, i, j, 0)) 
+			{
+                return true;
+            }
+        }
     }
     return false;
 }
 
-int main(void)
-{
-    ios_base::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
+int main(void) {
     
-    int C, N;
-    string Word;
-    
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
     cin >> C;
-    
-    for(int i = 0; i < C; i++)
-    {
-        for(int row = 0; row < 5; row++)
-        {
-            cin >> Board[row];
+
+    while(C--)
+	{
+
+		for(int i = 0; i < 5; i++) 
+		{
+            for(int j = 0; j < 5; j++) 
+			{
+                char c;
+                cin >> c;
+                board[i][j] = c;
+            }
         }
+
         cin >> N;
-    
-        for(int i = 0; i < N; i++)
-        {
-            bool flag = false;
-            cin >> Word;
-            for(int x = 0; x < 5; x++)
-                for(int y = 0; y < 5; y++)
-                {
-                    if(HasWord(x, y, Word))
-                    {
-                        flag = true;
-                        break;
-                    }
-                }
-            if(flag)
-            {
-                cout << Word << " " << "YES" << endl;
-            }
-            else
-            {
-                cout << Word << " " << "NO" << endl;
-            }
+
+        for(int i = 0; i < N; i++) 
+		{
+            memset(dp, -1, sizeof(dp));
+            string s;
+            cin >> s;
+            bool res = solution(s);
+            cout << s << " " << ((res) ? "YES" : "NO") << endl;
         }
     }
-    
+
     return 0;
 }
