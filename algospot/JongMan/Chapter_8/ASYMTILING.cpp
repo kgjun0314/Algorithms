b490feba2c6f20affcf3db0ff4db603ad5e1a373
kgjun0314 <unicorn9717@gmail.com>
2022-12-28 14:18:55 +0900

    Update ASYMTILING.cpp

---
+++ algospot/JongMan/Chapter_8/ASYMTILING.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+#include <limits>
+using namespace std;
+
+const long long MOD = 1000000007;
+long long dp[200];
+long long C, n;
+
+long long tiling(long long width)
+{
+	if(width <= 1)
+		return 1;
+	if(dp[width] != -1)
+		return dp[width];
+	return dp[width] = (tiling(width - 2) + tiling(width - 1)) % MOD;
+}
+
+long long asymmetric(long long width)
+{
+	if(width % 2 == 1)
+	{
+		return (tiling(width) - tiling(width / 2) + MOD) % MOD;
+	}
+	dp[width] = tiling(width);
+	dp[width] = (dp[width] - tiling(width/2) + MOD) % MOD;
+	dp[width] = (dp[width] - tiling(width/2 - 1) + MOD) % MOD;
+	return dp[width];
+}
+
+int main(void)
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	cin >> C;
+
+	while(C--)
+	{
+		cin >> n;
+		memset(dp, -1, sizeof(dp));
+		cout << asymmetric(n) << endl;
+	}
+
+	return 0;
+}
