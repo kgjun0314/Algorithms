b6c7fb751b4dc38e593fef61f024fc2f9576e07a
kgjun0314 <64140528+kgjun0314@users.noreply.github.com>
2023-01-12 00:01:19 +0900

    Update 1158.cpp

---
+++ acmicpc/1158.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+#include <cstring>
+#include <algorithm>
+#include <utility>
+#include <list>
+using namespace std;
+
+int C, N, K;
+
+void josephus(int n, int k)
+{
+	list<int> survivors;
+	for(int i = 1; i <= n; i++)
+	{
+		survivors.push_back(i);
+	}
+	list<int>::iterator kill = survivors.begin();
+	for(int i = 0; i < k - 1; i++)
+	{
+		kill++;
+		if(kill == survivors.end())
+		{
+			kill = survivors.begin();
+		}
+	}
+	cout << "<";
+	while(n > 1)
+	{	
+		cout << *kill << ", ";
+		kill = survivors.erase(kill);
+		if(kill == survivors.end())
+		{
+			kill = survivors.begin();
+		}
+		n--;
+		for(int i = 0; i < k - 1; i++)
+		{
+			kill++;
+			if(kill == survivors.end())
+			{
+				kill = survivors.begin();
+			}
+		}
+	}
+
+	cout << survivors.back() << ">" << endl;
+}
+
+int main(void)
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+	cin >> N >> K;
+	if(N == 1)
+	{
+		cout << "<1>" << endl;
+	}
+	else
+		josephus(N, K);
+	
+    return 0;
+}
