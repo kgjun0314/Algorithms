ce6b1363e331f49268b0e6aa6248468d32038e86
kgjun0314 <64140528+kgjun0314@users.noreply.github.com>
2023-02-08 21:32:20 +0900

    Edit 2667.cpp

--- acmicpc/2667.cpp
+++ acmicpc/2667.cpp
@@ -31,17 +31,15 @@ void DFS(int y, int x)
 		int ny = y + dy[i];
 		int nx = x + dx[i];
 
-		if(ny >= N || nx >= N || ny < 0 || nx < 0)
-		{
-			continue;
-		}
-
-		if(Map[ny][nx] == 1 && visited[ny][nx] == 0) // Map이 0인 부분 발견하면 DFS X
-		{
-			visited[ny][nx] = 1;
-			cnt++;
-			DFS(ny, nx);
-		}
+        if(ny < N && nx < N && ny >= 0 && nx >= 0)
+        {
+            if(Map[ny][nx] == 1 && visited[ny][nx] == 0)
+            {
+                visited[ny][nx] = 1;
+                cnt++;
+                DFS(ny, nx);
+            }
+        }
 	}
 }
 
@@ -92,7 +90,7 @@ int main(void)
 	{
 		for(int j = 0; j < N; j++)
 		{
-			if(Map[i][j] == 1 && visited[i][j] == 0) // 새로운 단지 발견
+			if(Map[i][j] == 1 && visited[i][j] == 0)
 			{
 				visited[i][j] = 1;
 				cnt = 1;
