905f002b9d45fa053a9bbbaca1a7e4ba1ecb0764
kgjun0314 <64140528+kgjun0314@users.noreply.github.com>
2023-02-19 17:58:42 +0900

    Update 15650.cpp

---
+++ acmicpc/15650.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <vector>
+#include <cstring>
+#include <algorithm>
+#include <utility>
+#include <list>
+#include <queue>
+#include <stack>
+#include <map>
+#include <utility>
+#include <sstream>
+#include <cstdlib>
+#include <cmath>
+#define endl "\n"
+using namespace std;
+
+int arr[9];
+bool visited[9];
+vector<int> vec;
+int N, M;
+
+void backtracking(int low, int cnt)
+{
+	if(cnt == M)
+	{
+		for(int i = 0; i < vec.size(); i++)
+		{
+			cout << vec[i] << " ";
+		}
+		cout << endl;
+		return;
+	}
+
+	for(int i = low; i < N; i++)
+	{
+		if(visited[i])
+			continue;
+		visited[i] = true;
+		vec.push_back(arr[i]);
+		backtracking(i + 1, cnt + 1);
+		vec.pop_back();
+		visited[i] = false;
+	}
+}
+
+int main(void)
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+	cin >> N >> M;
+
+	for(int i = 0; i < N; i++)
+	{
+		arr[i] = i + 1;
+		visited[i] = false;
+	}
+
+	backtracking(0, 0);
+
+    return 0;
+}
