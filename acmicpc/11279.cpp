bdccfedaf22fd185232ec479a9e6820ac507eb08
kgjun0314 <64140528+kgjun0314@users.noreply.github.com>
2023-02-26 21:09:22 +0900

    "Update 11279.cpp"

---
+++ acmicpc/11279.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <cstring>
+#include <algorithm>
+#include <utility>
+#include <list>
+#include <queue>
+#include <stack>
+#include <map>
+#include <utility>
+#include <sstream>
+#include <cstdlib>
+#define endl "\n"
+using namespace std;
+
+int N;
+int cmd;
+priority_queue<int, vector<int>, less<int> > pq;
+
+int main(void)
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+	cin >> N;
+	while(N--)
+	{
+		cin >> cmd;
+		if(cmd == 0)
+		{
+			if(pq.empty())
+			{
+				cout << 0 << endl;
+			}
+			else
+			{
+				cout << pq.top() << endl;
+				pq.pop();
+			}
+		}
+		else
+		{
+			pq.push(cmd);
+		}
+	}
+	
+    return 0;
+}
