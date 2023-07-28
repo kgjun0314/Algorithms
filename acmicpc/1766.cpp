e0f6d0257b4602475e3e3cea13c2bd5cce8e1a6f
kgjun0314 <unicorn9717@gmail.com>
2023-07-28 16:09:33 +0900

    Update 1766.cpp

---
+++ acmicpc/1766.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <utility>
+#include <queue>
+#define endl "\n";
+using namespace std;
+
+int N, M, A, B;
+vector<int> graph[32001];
+priority_queue<int, vector<int>, greater<int> > pq;
+vector<int> sorted;
+int indegree[32001];
+
+void topologicalSort()
+{
+    for(int i = 1; i <= N; i++)
+    {
+        if(indegree[i] == 0)
+            pq.push(i);
+    }
+
+    for(int i = 1; i <= N; i++)
+    {
+        int popnum = pq.top();
+        pq.pop();
+        sorted.push_back(popnum);
+        for(int j = 0; j < graph[popnum].size(); j++)
+        {
+            if(--indegree[graph[popnum][j]] == 0)
+                pq.push(graph[popnum][j]);
+        }
+    }
+}
+
+int main(void)
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    memset(indegree, 0, sizeof(indegree));
+
+    cin >> N >> M;
+
+    for(int i = 0; i < M; i++)
+    {
+        cin >> A >> B;
+        graph[A].push_back(B);
+        indegree[B]++;
+    }
+
+    topologicalSort();
+
+    for(int i = 0; i < sorted.size(); i++)
+    {
+        cout << sorted[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
