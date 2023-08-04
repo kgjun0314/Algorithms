7de58f19311eec7fa341d8d425a7c75eaa5ad511
김경준 <kgj0314@hanyang.ac.kr>
2023-08-04 20:11:36 +0900

    Edit 2042.cpp

--- acmicpc/2042.cpp
+++ acmicpc/2042.cpp
@@ -1,74 +1,73 @@
 #include <iostream>
-#include <cstring>
 #include <vector>
-#include <algorithm>
-#include <utility>
-#include <queue>
-#include <cmath>
-#include <stack>
-#define endl "\n";
+#define endl '\n'
 using namespace std;
 
-int N, M, K, a, b, c, idx, head, tail;
-long long x, value;
-long long arr[1000001], tree[1000001];
+const int MOD = 1000000007;
 
-long long prefixSum(int i)
+int N, M, K, cmd, b;
+long long num, c;
+vector<long long> tree;
+vector<long long> arr;
+
+long long init(int node, int start, int end) 
 {
-    long long result = 0;
-    while(i > 0)
-    {
-        result += tree[i];
-        i -= (i & -i);
-    }
-    return result;
+    if (start == end)
+        return tree[node] = arr[start];
+
+    int mid = (start + end) / 2;
+    return tree[node] = init(node * 2, start, mid) + init(node * 2 + 1, mid + 1, end);
 }
 
-void update(int i, long long dif)
+long long query(int node, int start, int end, int left, int right)
 {
-    while(i <= N)
-    {
-        tree[i] += dif;
-        i += (i & -i);
-    }
+    if (right < start || left > end)
+        return 0;
+
+    if (left <= start && end <= right) 
+        return tree[node];
+
+    int mid = (start + end) / 2;
+    return query(node * 2, start, mid, left, right) + query(node * 2 + 1, mid + 1, end, left, right);
 }
 
-long long intervalSum(int tail, int head)
+long long update(int node, int start, int end, int index, long long value) 
 {
-    return prefixSum(head) - prefixSum(tail - 1);
+    if (index < start || index > end) 
+        return tree[node];
+
+    if (start == end) 
+        return tree[node] = value;
+
+    int mid = (start + end) / 2;
+    return tree[node] = update(node * 2, start, mid, index, value) + update(node * 2 + 1, mid + 1, end, index, value);
 }
 
-int main(void)
+int main(void) 
 {
     ios_base::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
 
     cin >> N >> M >> K;
 
-    for(int i = 1; i <= N; i++)
+    for (int i = 0; i < N; ++i) 
     {
-        cin >> x;
-        arr[i] = x;
-        update(i, x);
+        cin >> num;
+        arr.push_back(num);
     }
-    int count = M + K;
-    while(count--)
+
+    tree.resize(N * 4);
+    init(1, 0, N - 1);
+
+    for (int i = 0; i < M + K; ++i) 
     {
-        int op;
-        cin >> op;
-        if(op == 1)
-        {
-            cin >> idx >> value;
-            update(idx, value - arr[idx]);
-            arr[idx] = value;
-        }
-        else
-        {
-            cin >> tail >> head;
-            cout << intervalSum(tail, head) << endl;
-        }
+        cin >> cmd >> b >> c;
+        if (cmd == 1) 
+            update(1, 0, N - 1, b - 1, c);
+        else if (cmd == 2) 
+            cout << query(1, 0, N - 1, b - 1, c - 1) << endl;
     }
-    
+
     return 0;
 }
